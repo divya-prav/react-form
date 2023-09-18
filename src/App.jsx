5c320671547f49237dfeb76ed83c2202850ec816
divya040392 <divya.praveen.5215@gmail.com>
2023-09-17 22:41:12 -0500

    done-working form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
+import { useState } from 'react'
 import './App.css'
+import SignUpForm from './assets/component/SignUpForm'
+import Authenticate from './assets/component/Authenticate';
 
 function App() {
- 
+   const [token,setToken] = useState(null);
 
   return (
     <>
-     
+     <SignUpForm token={token} setToken={setToken}/>
+     <Authenticate token={token} setToken={setToken}/>
     </>
   )
 }
